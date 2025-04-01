9dfc0d568ba48a3d9265d00bb1af8e458e16b637
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2025-04-01 11:41:01 +0900

    [Bronze V] Title: A×B, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Baekjoon
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+			Console.WriteLine(n[0] * n[1]);
+		}
+	}
+}
