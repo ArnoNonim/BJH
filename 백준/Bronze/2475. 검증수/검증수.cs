5c071d508c96173ab35dd720ff6e9e532cf7893e
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2025-10-02 12:47:53 +0900

    [Bronze V] Title: 검증수, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BaekJoon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			int sum = 0;
+
+			for(int i = 0; i < arr.Length; i++)
+			{
+				sum += arr[i] * arr[i];
+			}
+
+			Console.WriteLine(sum%10);
+
+		}
+	}
+}
