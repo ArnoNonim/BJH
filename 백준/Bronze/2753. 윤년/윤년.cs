9307924fbc5327cfe726f2ed6b17aa48ae388451
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2025-10-02 12:57:29 +0900

    [Bronze V] Title: 윤년, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BaekJoon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			if(n % 4 == 0 && (n % 100 != 0 || n % 400 == 0))
+			{
+				Console.WriteLine(1);
+			}
+			else
+			{
+				Console.WriteLine(0);
+			}
+		}
+	}
+}
