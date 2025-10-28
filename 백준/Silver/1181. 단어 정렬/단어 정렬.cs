ce849af23a07a581e03c26ca1e2125bb588f97a5
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2025-10-28 10:32:51 +0900

    [Silver V] Title: 단어 정렬, Time: 640 ms, Memory: 10712 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+namespace BaekJoon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			string[] words = new string[n];
+			for (int i = 0; i < n; i++)
+				words[i] = Console.ReadLine();
+
+			words = words.Distinct().OrderBy(w => w.Length).ThenBy(w => w).ToArray();
+
+			foreach (string word in words)
+				Console.WriteLine(word);
+		}
+	}
+}
