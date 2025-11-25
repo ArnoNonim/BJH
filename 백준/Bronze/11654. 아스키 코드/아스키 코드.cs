d3006f61e8b61b845db0cb71f6525a2cfc7c600c
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2025-11-25 14:13:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 44 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,11 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char n = char.Parse(Console.ReadLine());
+            Console.WriteLine((int)n);
+		}
+    }
+}
