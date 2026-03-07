e5cb3cc20d72c87feb29faa92b2c443a15aac632
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2026-03-07 11:05:18 +0900

    [Platinum V] Title: 약 팔기, Time: 104 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int n = int.Parse(Console.ReadLine());
+		Console.WriteLine(2000);
+		for (int i = 0; i < 1000; i++)
+			Console.Write("1 ");
+		for (int i = 0; i < 1000; i++)
+			Console.Write("1000 ");
+	}
+}
