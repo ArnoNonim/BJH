21bb0a4b55039bf9956c8ff683d63807a4031671
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2026-03-07 10:52:03 +0900

    [Bronze V] Title: SciComLove, Time: 48 ms, Memory: 5496 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int n = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i < n; i++) Console.WriteLine("SciComLove");
+	}
+}
