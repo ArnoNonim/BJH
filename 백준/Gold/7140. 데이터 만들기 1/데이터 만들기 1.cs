08ccfd38605204d7958f8d0db5c715aeb7746c2a
ArnoNonim <66023091+ArnoNonim@users.noreply.github.com>
2026-03-20 16:47:14 +0900

    [Gold IV] Title: 데이터 만들기 1, Time: 48 ms, Memory: 5412 KB -BaekjoonHub

---
+++ 백준/Gold/7140. 데이터 만들기 1/데이터 만들기 1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Baekjoon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(101);
+			for(int i = 0; i < 101; i++)
+			{
+				Console.WriteLine(0);
+			}
+			Console.WriteLine($"1\n0 1");
+		}
+	}
+}
